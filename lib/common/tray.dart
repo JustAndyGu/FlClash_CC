949c0ef021832c99754c7b7eb089e73d4c214320
guh <andygu@up.com>
2025-12-15 12:40:56 +0800

    fix: fix bugs

--- lib/common/tray.dart
+++ lib/common/tray.dart
@@ -4,6 +4,7 @@ import 'package:fl_clash/common/iterable.dart';
 import 'package:fl_clash/enum/enum.dart';
 import 'package:fl_clash/models/models.dart';
 import 'package:fl_clash/state.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:tray_manager/tray_manager.dart';
